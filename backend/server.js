36bad8e2b58d22cb6a3364cb313fc1911a9499bd
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-01-20 08:09:42 +0530

    user route and controller file added

--- backend/server.js
+++ backend/server.js
@@ -6,3 +6,9 @@ const port = process.env.PORT || 5000;
 const app = express();
 app.get('/', (req, res) => res.send('Server is ready'));
 app.listen(port, () => console.log(`Server started on port ${port}`));
+
+// -**Post /api/users** - Register a uster
+// -**Post /api/users/auth** - Authenticate a user and get a token
+// -**Post /api/users/logout** - Logout a user and clear cookie
+// -**GET /api/users/profile** - Get user profile
+// -**PUT /api/users/profile** - Update user profile
