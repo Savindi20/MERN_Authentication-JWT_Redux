400783a82b9dffb907b5cf6a75217c804de816cf
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-01-21 18:04:18 +0530

    register user updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
 
 // @desc    Auth user/set token
 // @route   POST /api/users/auth
@@ -13,6 +14,8 @@ const authUser = asyncHandler(async(req, res) => {
 // @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async(req, res) => {
+    console.log(req.body);
+
     res.status(200).json({ message: 'Register User' });
 });
 
