873fb92297e832542f684e55d2de63c02d006b25
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-02-03 13:40:06 +0530

    api slice pade added and updated

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './features/auth/authSlice';
+import authReducer from './slices/authSlice';
+import { apiSlice } from './slices/apiSlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware(),
