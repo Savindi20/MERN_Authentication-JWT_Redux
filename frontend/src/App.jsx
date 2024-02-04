008d5d3aa8d12013e08f6a193218b8147293dd05
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-02-04 10:27:11 +0530

    project refactored

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,19 @@
-import { Outlet } from 'react-router-dom'
-import { Container } from 'react-bootstrap'
-import Header from './components/Header'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <>
       <Header />
       <ToastContainer />
-      {/* <Container className='my-2'>
+      <Container className='my-2'>
         <Outlet />
-      </Container> */}
-      <Outlet />
+      </Container>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,11 +4,11 @@ import { apiSlice } from './slices/apiSlice';
 
 const store = configureStore({
   reducer: {
-    auth: authReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware(),
+    getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true,
 });
 
